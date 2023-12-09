13324b51709512418b460d8d0a9a07637b1ff899
Duncan Miller <dmiller@heatspring.com>
2023-12-08 16:59:29 -0800

    adding article model

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    body = models.TextField()
+    published_at = models.DateTimeField()
