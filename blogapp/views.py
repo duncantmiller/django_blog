ede614e5730e50e56a6e92f4292e38e8219c7947
Duncan Miller <dmiller@heatspring.com>
2023-12-08 17:14:11 -0800

    add article show view that gets passed the article

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Article
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def show(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    return render(request, "show.html", {"article": article})
