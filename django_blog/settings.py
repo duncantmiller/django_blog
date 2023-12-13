0692cec542386cf8f765a226b3b78cc63f734676
Duncan Miller <dmiller@heatspring.com>
2023-12-13 15:06:26 -0800

    change app name to blog

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blogapp.apps.BlogappConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
