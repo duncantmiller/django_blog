95946b4828dc6de62808150c046a7d5cff6a9138
Duncan Miller <dmiller@heatspring.com>
2023-12-13 15:49:47 -0800

    add author relationship and set up first article

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Article
 
 # Create your views here.
 def index(request):
-    articles = Article.objects.order_by("-published_at")
+    articles = Article.objects.order_by("published_at")
     return render(request, 'index.html', {"articles": articles})
 
 def show(request, article_id):
