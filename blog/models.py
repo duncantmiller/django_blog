c2bf79c33eaf3deff0e917f602531eb0ed5cb59d
Duncan Miller <dmiller@heatspring.com>
2023-12-13 15:35:23 -0800

    add author model

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
     body = models.TextField()
     published_at = models.DateTimeField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="articles")
 
     def __str__(self) -> str:
         return f"{self.id}: {self.title}"
