3114dc5fc501808982292fd1db3bf8ae035a05e5
Duncan Miller <dmiller@heatspring.com>
2023-12-13 17:04:10 -0800

    configure admin panel for filter horizontal tags

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ("tags",)
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
