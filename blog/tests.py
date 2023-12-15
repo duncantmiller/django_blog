e4e2dac024ebf2b3926e6254ff339ad95698c214
Duncan Miller <dmiller@heatspring.com>
2023-12-15 14:09:11 -0800

    fix test name and add test to make sure article page renders

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from blog.models import *
 
 class ArticleTestCase(TestCase):
@@ -16,8 +16,13 @@ class ArticleTestCase(TestCase):
     def test_comments_count(self):
         self.assertEqual(self.article.comments_count(), 1)
 
-    def has_comments(self):
+    def test_has_comments(self):
         self.assertTrue(self.article.has_comments())
+
+    def test_has_valid_article_page(self):
+        c = Client()
+        response = c.get(f"/articles/{self.article.id}/")
+        self.assertEqual(response.status_code, 200)
 
 class AuthorTestCase(TestCase):
 
